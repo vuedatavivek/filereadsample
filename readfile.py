3b664645f6c5d94c9e61b62772b3be9e48f61c21
Vivek Manoharan <vivek.m@vuedata.in>
2022-10-10 12:21:33 +0530

    sample code update 10-10-2022

--- readfile.py
+++ readfile.py
@@ -1,7 +1,5 @@
-from dataclasses import fields
 import glob
 import json
-from operator import countOf
 import pandas as pd
 import csv
 def createdatamodel(row, columns):
@@ -15,15 +13,21 @@ def filedata(filename):
     columns = []
     rows = []
     with open(filename, 'r') as csvfile:
-        csvreader = csv.reader(csvfile)
-        columns = next(csvreader)
-        for row in csvreader:
-            rows.append(row)
-            dataobj = createdatamodel(row, columns)
-            dataset.append(dataobj)
+        df = pd.read_csv(csvfile)
+        columns = pd.Series(df)
+        #json_obj = df.head().to_json()
+        #json_obj = df.describe().to_json()
+        
+        # json_obj = df.describe().to_json()
+        json_obj = columns
+        print(json_obj)
+        # for row in csvreader:
+        #     rows.append(row)
+        #     dataobj = createdatamodel(row, columns)
+        #     dataset.append(dataobj)
 
-        print("Total no of rows: %d"%(csvreader.line_num))
-        print("Total no of columns: %d"%(len(columns)))
+        # print("Total no of rows: %d"%(csvreader.line_num))
+        # print("Total no of columns: %d"%(len(columns)))
         # json_obj = json.dumps(dataset)
         # with open('dictionary.json', 'w') as outfile:
         #     outfile.write(json_obj)
@@ -31,15 +35,17 @@ def filedata(filename):
 def main():
     path = 'csvfiles'
     files = glob.glob(path+"/*.csv")
-    dataframe = pd.DataFrame()
-    content = []
+    datadis = {}
     for filename in files:
-        print(filename)
-        filedata(filename)
-        df = pd.read_csv(filename, index_col=None)
-        df.shape
-        content.append(df)
-    dataframe = pd.concat(content)
+        data = filedata(filename)
+        datadis[filename] = data
+    json_obj = json.dumps(datadis)
+    with open('dictionary.json', 'w') as outfile:
+        outfile.write(json_obj)
+    #     df = pd.read_csv(filename, index_col=None)
+    #     df.shape
+    #     content.append(df)
+    # dataframe = pd.concat(content)
 
     #print(content.index)
     #print(dataframe)
--- samplecode.py
+++ samplecode.py
@@ -1,3 +1,28 @@
+#10-10-2022
+def fib_generator(limit):
+    a = 0
+    b = 1
+    output = []
+    if limit < 0:
+        print("Incorrect input")
+    elif limit == 0:
+        return [a]
+    elif limit == 1:
+        return [a,b]
+    else:
+        output.append(a)
+        for i in range(0, limit):            
+            c = a + b
+            a = b
+            b = c
+            output.append(a)
+        return output
+# Driver Program
+limit = int(input("Enter the limit\n"))
+for i in range(0, limit):
+    c = fib_generator(i)
+    print("limit = %d"%(i), c)
+
 
 # import math
 # import pandas as pd
@@ -14,14 +39,14 @@
 
 # myset = set(["Geeks", "For", "Geeks"])
 # print(myset)
-student_id = ["S004", "S005", "S003", "S001", "S002"]
-student_name = ["MS Dohni", "Ajith Kumar", "Cristiano Ronaldo", "David Beckham", "Sachin"]
-student_grade = [85, 98, 89, 92, 100]  
-output = []
-for idx, id in enumerate(student_id):
-    stu_obj = {id:{student_name[idx]: student_grade[idx]}}
-    output.append(stu_obj)
-print(output)
+# student_id = ["S004", "S005", "S003", "S001", "S002"]
+# student_name = ["MS Dohni", "Ajith Kumar", "Cristiano Ronaldo", "David Beckham", "Sachin"]
+# student_grade = [85, 98, 89, 92, 100]  
+# output = []
+# for idx, id in enumerate(student_id):
+#     stu_obj = {id:{student_name[idx]: student_grade[idx]}}
+#     output.append(stu_obj)
+# print(output)
 
 
 
